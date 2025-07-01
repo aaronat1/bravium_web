b2ba079acc1d0dff9ba944ab6dc852e9f352c0e0
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2025-07-01 17:22:15 +0000

    tengo este error al crear un cliente: FirebaseError: Missing or insuffic

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,20 +1,29 @@
 import admin from 'firebase-admin';
 import { getApps } from 'firebase-admin/app';
 
-const serviceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-};
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 if (!getApps().length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  } catch (error) {
-    console.error("Firebase admin initialization error", error);
-  }
+    if (!projectId || !clientEmail || !privateKey) {
+        console.error('-------------------------------------------------------------------------------------------------');
+        console.error('ERROR: Las credenciales del SDK de Firebase Admin no están configuradas.');
+        console.error('Asegúrate de tener un fichero .env con las variables NEXT_PUBLIC_FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL y FIREBASE_PRIVATE_KEY.');
+        console.error('-------------------------------------------------------------------------------------------------');
+    } else {
+      try {
+        admin.initializeApp({
+          credential: admin.credential.cert({
+            projectId,
+            clientEmail,
+            privateKey,
+          }),
+        });
+      } catch (error) {
+        console.error("Error al inicializar Firebase Admin:", error);
+      }
+    }
 }
 
 export const adminAuth = admin.auth();
